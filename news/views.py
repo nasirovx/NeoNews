adedd505c2aa59a60e606d4f8d56462e201982eb
5ekastan <151753486+nasirovx@users.noreply.github.com>
2024-07-17 08:36:18 +0300

    Generic api + docker 🦄

--- news/views.py
+++ news/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 from django.contrib.auth import authenticate
 from drf_yasg.utils import swagger_auto_schema
 from .serializers import *
